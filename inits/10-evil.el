45d2bd95ed4324a3687b3c6d254ac77116abe846
Eitaro Fukamachi <e.arrows@gmail.com>
2014-12-28 14:34:31 +0900

    Define a keybind C-d as delete-char in Evil's insert-mode.

--- inits/10-evil.el
+++ inits/10-evil.el
@@ -5,6 +5,7 @@
 (define-key evil-normal-state-map (kbd "C-e") 'move-end-of-line)
 (define-key evil-insert-state-map (kbd "C-k") 'previous-line)
 (define-key evil-insert-state-map (kbd "C-j") 'next-line)
+(define-key evil-insert-state-map (kbd "C-d") 'delete-char)
 
 (define-key evil-normal-state-map "D"
   (lambda ()
