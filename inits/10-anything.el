08209aa03847b4efe80fd1a1f716aba2a8a8d097
Eitaro Fukamachi <e.arrows@gmail.com>
2015-01-26 10:40:58 +0900

    Don't use anything.el for finding a file (C-x C-f).

--- inits/10-anything.el
+++ inits/10-anything.el
@@ -1,6 +1,5 @@
 (require-or-install 'anything)
 (require-or-install 'anything-startup)
-(global-set-key (kbd "C-x C-f") 'anything-find-file)
 (global-set-key (kbd "C-x b") 'anything)
 (global-set-key (kbd "M-x") 'anything-execute-extended-command)
 (define-key anything-map "\C-j" 'anything-next-line)
