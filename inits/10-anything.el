5a6b5a44922620fc59f9017c2355180a21dc3cba
Eitaro Fukamachi <e.arrows@gmail.com>
2015-01-29 16:22:00 +0900

    Set anything-find-file on C-x C-f again.

--- inits/10-anything.el
+++ inits/10-anything.el
@@ -1,5 +1,6 @@
 (require-or-install 'anything)
 (require-or-install 'anything-startup)
+(global-set-key (kbd "C-x C-f") 'anything-find-file)
 (global-set-key (kbd "C-x b") 'anything)
 (global-set-key (kbd "M-x") 'anything-execute-extended-command)
 (define-key anything-map "\C-j" 'anything-next-line)
